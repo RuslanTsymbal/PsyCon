a706ad3ef911604852d0d69f2b0ea7afd33e85e2
RuslanTsymbal <russslan.ts@gmail.com>
2017-07-05 18:06:14 +0300

    all-ready+360

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,10 @@
 $(document).ready(function() {
 
-  var widthParent_1 = $('.chat-pricing-cell').width();
-  var widthlement_1 = $('.chat-pricing-cell__min').width();
-  var styleleft_1 = (widthParent_1 - widthlement_1 ) / 2 + 'px';
-
-  $('.chat-pricing-cell__min').css("left", styleleft_1);
-
-
-  var widthParent_2 = $('.chat-pricing-cell-little').width();
-  var widthlement_2 = $('.chat-pricing-cell__min-little').width();
-  var styleleft_2 = (widthParent_2 - widthlement_2 ) / 2 + 'px';
-
-  $('.chat-pricing-cell__min-little').css("left", styleleft_2);
-
-
-
   //add class if width < 769px
   var width = $('body').innerWidth();
+
   if (width < 769) {
-      $(".page-header__nav").addClass("nav-mob");
+    $(".page-header__nav").addClass("nav-mob");
   } else {
     $(".page-header__nav").removeClass("nav-mob");
   }
@@ -56,16 +42,16 @@ $(document).ready(function() {
       var docHeight = $(document).height();
       $("body").append("<div id='overlay'></div>");
       $("#overlay")
-          .height(docHeight)
-          .css({
-            'opacity' : 0.4,
-            'position': 'absolute',
-            'top': 0,
-            'left': 0,
-            'background-color': 'black',
-            'width': '100%',
-            'z-index': 1
-        });
+        .height(docHeight)
+        .css({
+          'opacity' : 0.4,
+          'position': 'absolute',
+          'top': 0,
+          'left': 0,
+          'background-color': 'black',
+          'width': '100%',
+          'z-index': 1
+      });
     }
 
     if(statusFirst) {
@@ -110,6 +96,6 @@ $(document).ready(function() {
     }
   });
 
-   //Plugin select
+   //Plugin jquery "Select"
    $("select" ).selecter();
 });
